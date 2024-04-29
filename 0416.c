ef622791984e4c1c51797c194a07487330be9b92
chanwon05 <165672373+chanwon05@users.noreply.github.com>
2024-04-29 16:20:28 +0900

    Update 0416.c

--- 0416.c
+++ 0416.c
@@ -1,34 +1,37 @@
 // 백준 1546번
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {
-	int N, M;
-	int score[1000] = { 0, };
-	float sum = 0;
+	int N;
+	int arr[1000] = { 0, };
 	scanf("%d", &N);
-	
+
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%d", &score[i]);
+		scanf("%d", &arr[i]);
 	}
-	int max = score[0];
+
+	int max = arr[0];
+
 	for (int i = 1; i < N; i++)
 	{
-		if (max < score[i])
+		if (arr[i] > max)
 		{
-			max = score[i];
+			max = arr[i];
 		}
 	}
+
+	float result = 0;
+	float sum = 0;
+
 	for (int i = 0; i < N; i++)
 	{
-		sum += (float) score[i] / max * 100;
+		result = (float) arr[i] / max * 100;
+			sum += result;
 	}
 
-	printf("%f", sum/N);
-	
-
+	printf("%f", sum / N);
 
 	return 0;
 }
